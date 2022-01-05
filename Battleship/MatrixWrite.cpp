8237f86768fdcd7de6d7521bc6c45b54cce6c4d4
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2022-01-04 20:50:40 -0800

    Updated matrixWrite Parameters
    I did

--- Battleship/MatrixWrite.cpp
+++ Battleship/MatrixWrite.cpp
@@ -3,28 +3,29 @@
 namespace matrixwrite
 {
 	//Write a vector or matrix onto a matrix
-	int matrixWriteInt(int largeMatrix, int smallMatrix)
+	int matrixWriteInt(int largeMatrix, int smallMatrix, int inX, int inY, int inTheta, int largeWidth, int largeLength, int smallWidth, int smallLength)
 	{
+		
 		return 0;
 	};
 
-	bool matrixWriteBool(bool largeMatrix, bool smallMatrix)
+	bool matrixWriteBool(bool largeMatrix, bool smallMatrix, int inX, int inY, int inTheta, int largeWidth, int largeLength, int smallWidth, int smallLength)
 	{
 		return true;
 	};
 
-	char matrixWriteChar(char largeMatrix, char smallMatrix)
+	char matrixWriteChar(char largeMatrix, char smallMatrix, int inX, int inY, int inTheta, int largeWidth, int largeLength, int smallWidth, int smallLength)
 	{
 
 		return '_';
 	};
 
-	float matrixWriteFloat(float largeMatrix, float smallMatrix)
+	float matrixWriteFloat(float largeMatrix, float smallMatrix, int inX, int inY, int inTheta, int largeWidth, int largeLength, int smallWidth, int smallLength)
 	{
 		return 0;
 	};
 
-	double matrixWriteDouble(double largeMatrix, double smallMatrix)
+	double matrixWriteDouble(double largeMatrix, double smallMatrix, int inX, int inY, int inTheta, int largeWidth, int largeLength, int smallWidth, int smallLength)
 	{
 		return 0;
 	};
